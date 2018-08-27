602c4f9f3bd47ebe6f14ae1c946dffea875415a9
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-08-27 23:09:24 +0800

    Hi Voice

---
+++ HiMpge/HiMpge/Mp4Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HiMpge.HI;
+
+namespace HiMpge
+{
+    public partial class Mp4Player : Form
+    {
+        public Mp4Player()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var decAttr = new hi_h264dec.hiH264_DEC_ATTR_S();
+        }
+    }
+}
